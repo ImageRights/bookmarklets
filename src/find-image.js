2d4e2dc0a02dbb6f560c14deed9b5ee957c6e219
unknown <will.harney@imagerights.com>
2018-08-09 12:15:45 -0400

    Update find-image to take delay as user input.

--- src/find-image.js
+++ src/find-image.js
@@ -1,17 +1,30 @@
-;(function find (target, container) {
-  const elt = container.find(`.buttons-right a[href$="=${target}"]`)
-  if (!elt.length) {
-    const next = $('li:not(.disabled) > a:contains(Next)')
-    if (!next.length) return alert('Could not find target.')
-    next.click()
-    return setTimeout(find, 250, target, container)
+;(function () {
+  const idx = location.hash.indexOf('?')
+  const target = new URLSearchParams(location.hash.slice(idx)).get('highlight')
+  if (!target) {
+    return alert('Cannot search for missing target.')
   }
-  const thumb = elt.closest('.ir-thumbnail')
-  thumb.find('.glyphicon-ok').click()
-  thumb[0].scrollIntoView()
-})((s => {
-  const idx = s.indexOf('?')
-  const target = new URLSearchParams(s.slice(idx)).get('highlight')
-  if (!target) throw new Error('Cannot search for missing target.')
-  return target
-})(location.hash), $('.ir-thumbnail-container'))
+  let delay = prompt('Enter a delay in milliseconds:', 500)
+  if (!delay) {
+    return
+  }
+  delay |= 0
+  if (!delay) {
+    return alert('Invalid delay.')
+  }
+  const container = $('.ir-thumbnail-container')
+  return (function find () {
+    const elt = container.find(`.buttons-right a[href$="=${target}"]`)
+    if (elt.length) {
+      const thumb = elt.closest('.ir-thumbnail')
+      thumb.find('.glyphicon-ok').click()
+      return thumb[0].scrollIntoView()
+    }
+    const next = $('li:not(.disabled) > a:contains(Next)')
+    if (!next.length) {
+      return alert('Could not find target.')
+    }
+    next.click()
+    return setTimeout(find, delay, target, container, delay)
+  })()
+})()
