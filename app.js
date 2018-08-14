780583c413b555a465817abe3d0e051f16b2d0b9
Will Harney <will.harney@imagerights.com>
2018-08-14 17:42:19 -0400

    Removed unsafe: prefix from bmlets; moved inline js to own file.

---
+++ app.js
@@ -0,0 +1,22 @@
+;(function () {
+  'use strict'
+  var app = angular.module('bookmarklets', [])
+    .config([
+      '$compileProvider',
+      function ($compileProvider) {
+        var regex = /^\s*(https?|javascript):/
+        $compileProvider.aHrefSanitizationWhitelist(regex)
+      }
+    ])
+  app.controller('main', function ($scope, $http) {
+    angular.element('a[ng-href]').each(function (idx, elt) {
+      var name = angular.element(elt).attr('ng-href').slice(2, -2)
+      if ($scope.hasOwnProperty(name)) {
+        throw new Error('Duplicate name: ' + name)
+      }
+      $http.get('dist/' + name + '.js').then(function (res) {
+        $scope[name] = res.data
+      })
+    })
+  })
+})()
