779f4d180d149a86dc4ddeb4a4d63192bc431ebe
Will Harney <will.harney@imagerights.com>
2018-08-23 17:51:47 -0400

    Update keyboard_verify to be more angular.

--- src/keyboard_verify.js
+++ src/keyboard_verify.js
@@ -1,29 +1,54 @@
 ;(() => {
   const delay = (d, v) => new Promise(resolve => setTimeout(resolve, d, v))
-  async function next () {
-    // Toggle All
-    const all = angular.element('.glyphicon-chevron-right[ng-click^=toggle_expand_all]')
-    if (all.length) {
-      all.triggerHandler('click')
-      await delay(200)
-    }
-    // Toggle Row
-    const row = angular.element('.ha-sighting-header').first().find('.glyphicon-chevron-right')
-    if (row.length) {
-      row.triggerHandler('click')
-      await delay(100)
-    }
-    const check = angular.element('.ha-sighting-row').first().find('input[type=checkbox]')
-    if (!check.prop('checked')) {
-      check.trigger('click') // triggerHandler doesn't work
+  async function next (tried) {
+    try {
+      // Toggle All
+      const all = angular.element('.glyphicon-chevron-right[ng-click^=toggle_expand_all]')
+      if (all.length) {
+        const $scope = all.scope()
+        $scope.toggle_expand_all()
+        $scope.$apply()
+        await delay(500)
+      }
+      // Toggle Row
+      const row = angular.element('.ha-sighting-header').first().find('.glyphicon-chevron-right')
+      if (row.length) {
+        const $scope = row.scope()
+        $scope.toggle_expand($scope.claim_group)
+        $scope.$apply()
+        await delay(150)
+      }
+      const check = angular.element('.ha-sighting-row').first().find('input[type=checkbox]')
+      if (!check.prop('checked')) {
+        const $scope = check.scope()
+        $scope.sight.select = true
+        $scope.apply_select($scope.claim_group)
+        $scope.$apply()
+      }
+    } catch (err) {
+      if (tried) {
+        throw err
+      } else {
+        return setTimeout(next, 150, true)
+      }
     }
   }
   next()
-  document.addEventListener('keyup', e => {
-    if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
-      const btn = e.key === 'ArrowLeft' ? '.btn-danger' : '.btn-success'
-      angular.element(btn).triggerHandler('click')
-      setTimeout(next, 500)
+  document.addEventListener('keyup', ($scope => e => {
+    const click = bool => {
+      if ($scope.selected_group) {
+        $scope.admin_verify_claims($scope.selected_group, false)
+        $scope.$apply()
+        setTimeout(next, 500)
+      } else {
+        setTimeout(next, 0)
+      }
     }
-  })
+    if (e.key === 'ArrowLeft') {
+      click(false)
+    } else if (e.key === 'ArrowRight') {
+      click(true)
+    }
+    // else ignore event
+  })(angular.element('.btn-success').scope()))
 })()
