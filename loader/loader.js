6ca0c5c9e639caef758ec1ccdbd9a7a54806a59b
Will Harney <will.harney@imagerights.com>
2018-09-11 15:43:20 -0400

    Update loader to *actually* point to rawgit.

--- loader/loader.js
+++ loader/loader.js
@@ -5,6 +5,6 @@
   var peer = d.getElementsByTagName(s)[0]
   var src = d.createElement(s)
   src.id = 'IR-bm-' + id
-  src.src = 'https://raw.githubusercontent.com/wjhir/bookmarklets/master/dist/' + id + '.js'
+  src.src = 'https://cdn.rawgit.com/wjhir/bookmarklets/master/dist/' + id + '.js'
   peer.parentNode.insertBefore(src, peer)
 }(document, 'script', '__ID__'))
