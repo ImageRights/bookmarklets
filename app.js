64f7f7ad0f1fbe4768103bee1754780654bd70e0
Will Harney <will.harney@imagerights.com>
2018-09-26 15:14:40 -0400

    Update app and loader to allow for static or dynamically loaded bmlets.

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 ;(function () {
   'use strict'
   var app = angular.module('bookmarklets', [])
-    .config([
-      '$compileProvider',
-      function ($compileProvider) {
-        var regex = /^\s*(https?|javascript):/
-        $compileProvider.aHrefSanitizationWhitelist(regex)
-      }
-    ])
-  app.controller('main', function ($scope, $http) {
-    angular.element('a[ng-href]').each(function (idx, elt) {
-      var name = angular.element(elt).attr('ng-href').slice(2, -2)
-      if ($scope.hasOwnProperty(name)) {
-        throw new Error('Duplicate name: ' + name)
-      }
-      $http.get('bmlets/' + name + '.url').then(function (res) {
-        $scope[name] = res.data
+  app.config([
+    '$compileProvider',
+    function ($compileProvider) {
+      var regex = /^\s*(https?|javascript):/
+      $compileProvider.aHrefSanitizationWhitelist(regex)
+    }
+  ])
+  app.directive('irBmlet', ['$http', function ($http) {
+    function load (url) {
+      return $http.get(url).then(function (response) {
+        return response.data
       })
-    })
-  })
+    }
+    var loader = load('./loader/loader.url')
+    function link (scope, elt, attrs) {
+      var info = attrs.irBmlet.split(' ')
+      var type = info[0]
+      var name = info[1]
+      if (type === 'load') {
+        return loader.then(function (bmlet) {
+          elt.attr('href', bmlet.replace('__ID__', name))
+        })
+      } else if (type === 'static') {
+        return load('./static/' + name + '.url').then(function (bmlet) {
+          elt.attr('href', bmlet)
+        })
+      } // Else shouldn't happen
+    }
+    return { restrict: 'A', link: link }
+  }])
 })()
