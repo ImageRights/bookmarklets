c78606f0814d46dbc326df289226e791f5e043f1
Will Harney <will.harney@imagerights.com>
2018-09-27 14:37:51 -0400

    Allow bundling multiple files and allow static or dynamic loading.

---
+++ controls/publish.js
@@ -0,0 +1,29 @@
+'use strict'
+const bundle = require('./util/bundle')
+const isDir = require('./util/is_directory')
+const {promisify} = require('util')
+const fs = require('fs')
+const write = promisify(fs.writeFile)
+const path = require('path')
+
+function parse (PARSER_FILE, IN_DIR, OUT_DIR, OUT_EXT, ...names) {
+  const parser = require('./util/' + PARSER_FILE)
+  return Promise.all(names.map(async function (name) {
+    const input = path.resolve(IN_DIR, name)
+    const output = path.resolve(OUT_DIR, name + OUT_EXT)
+    let code
+    if (await isDir(input)) {
+      const bundled = await bundle(input, OUT_DIR)
+      if (bundled !== output) {
+        throw new Error('Parcel bundled to unexpected location.')
+      }
+      code = await parser(output)
+    } else {
+      code = await parser(input + '.js')
+    }
+    await write(output, await code, 'utf8')
+    return true
+  }))
+}
+
+require('./util/call_or_export')(module, parse)
---
+++ controls/util/bundle.js
@@ -0,0 +1,21 @@
+// Bundle contents of a directory into one file
+'use strict'
+const Bundler = require('parcel-bundler')
+const path = require('path')
+module.exports = async function (indir, outdir) {
+  const options = {
+    outDir: outdir, // The out directory to put the build files in, defaults to dist
+    outFile: path.basename(indir) + '.js', // The name of the outputFile
+    watch: false, // whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
+    cache: false, // Enabled or disables caching, defaults to true
+    contentHash: false, // Disable content hash from being included on the filename
+    minify: true, // Minify files, enabled if process.env.NODE_ENV === 'production'
+    target: 'browser', // browser/node/electron, defaults to browser
+    logLevel: 3, // 3 = log everything, 2 = log warnings & errors, 1 = log errors
+    hmr: false, // Enable or disable HMR while watching
+    sourceMaps: false // Enable or disable sourcemaps, defaults to enabled (not supported in minified builds yet)
+  }
+  // I don't think there's an option to avoid writing to a file :|
+  const bundle = await new Bundler(require.resolve(indir), options).bundle()
+  return bundle.name // Output filepath
+}
