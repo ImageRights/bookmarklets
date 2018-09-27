a8ed659bf81891835cfebd7c820c9b903ead7733
Will Harney <will.harney@imagerights.com>
2018-09-27 17:19:13 -0400

    Update date finder json-ld search to accommodate malformed JSON.

--- src/published_date/search/ldJson.js
+++ src/published_date/search/ldJson.js
@@ -1,24 +1,32 @@
 // Search in JSON-LD script tags
 'use strict'
 const MultiError = require('../util/MultiError')
+const singleMatch = require('../util/singleMatch')
 module.exports = function ldJson () {
   const scr = document.querySelectorAll('script[type="application/ld+json"]')
   let date = null
   for (const s of scr) {
     let json
+    let found
+    const text = s.textContent.trim()
     try {
-      json = JSON.parse(s.textContent.trim())
+      json = JSON.parse(text)
+      found = json.datePublished || json.dateModified
     } catch (e) {
-      continue
+      // Check malformed JSON
+      const match = singleMatch(/"datePublished":\s*?"([^"]+?)"/, text) ||
+        singleMatch(/"datePublished":\s*?"([^"]+?)"/, text)
+      if (match) {
+        found = match[1]
+      }
     }
-    let d = json.datePublished || json.dateModified
-    if (d) {
+    if (found) {
       if (date) {
-        if (date === d) {
+        if (date === found) {
           throw new MultiError()
         }
       } else {
-        date = d
+        date = found
       }
     }
   }
