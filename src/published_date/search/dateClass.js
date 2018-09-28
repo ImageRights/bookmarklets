f9acd4e7607bfbd2d1923c231af23792a26c9034
Will Harney <will.harney@imagerights.com>
2018-09-28 12:38:27 -0400

    Add search for "date" class.

---
+++ src/published_date/search/dateClass.js
@@ -0,0 +1,8 @@
+// Searches for elements with "date" in a class name
+'use strict'
+const query = require('./query')
+const text = require('./text')
+module.exports = function () {
+  const str = query('[class*=date]', null, 'textContent')
+  return text(str) || str
+}
