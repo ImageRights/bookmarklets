db65514ccd1c2be43f6b50458a3b6541b65d6cf7
Will Harney <will.harney@imagerights.com>
2018-10-01 13:51:26 -0400

    Remove broken already-loaded check.

--- loader/loader.js
+++ loader/loader.js
@@ -1,10 +1,6 @@
 (function (d, s, id, path) {
-  if (d.getElementById(id)) {
-    return alert('Already loaded boomkarklet.')
-  }
   var peer = d.getElementsByTagName(s)[0]
   var src = d.createElement(s)
-  src.id = 'IR-bm-' + id
   src.src = path + id + '.js'
   peer.parentNode.insertBefore(src, peer)
 }(document, 'script', '__ID__', '__PATH__'))
