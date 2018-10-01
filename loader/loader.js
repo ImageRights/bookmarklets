4553ad59781734920741e74cf57d2be81d3a037f
Will Harney <will.harney@imagerights.com>
2018-10-01 13:54:33 -0400

    Remove requirement for a page to already have a script tag.

--- loader/loader.js
+++ loader/loader.js
@@ -1,6 +1,5 @@
 (function (d, s, id, path) {
-  var peer = d.getElementsByTagName(s)[0]
   var src = d.createElement(s)
   src.src = path + id + '.js'
-  peer.parentNode.insertBefore(src, peer)
+  d.head.appendChild(src)
 }(document, 'script', '__ID__', '__PATH__'))
