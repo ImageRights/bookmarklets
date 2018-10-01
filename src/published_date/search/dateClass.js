f993529f25ee5d324826a851dce0f2297d45dcb7
Will Harney <will.harney@imagerights.com>
2018-10-01 16:17:01 -0400

    Fix date class search causing an error when no matching elements found.

--- src/published_date/search/dateClass.js
+++ src/published_date/search/dateClass.js
@@ -4,5 +4,5 @@ const query = require('./query')
 const text = require('./text')
 module.exports = function () {
   const str = query('[class*=date]', null, 'textContent')
-  return text(str) || str
+  return str ? text(str) || str : null
 }
