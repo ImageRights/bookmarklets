7c9b9cdfc1aa64e1e39ba262e00c4bd31a24038e
Will Harney <will.harney@imagerights.com>
2018-10-04 13:01:22 -0400

    Update loader to alert a message on error.

--- loader/loader.js
+++ loader/loader.js
@@ -1,5 +1,9 @@
 (function (d, s, id, path) {
   var src = d.createElement(s)
+  src.addEventListener('error', function (evt) {
+    const msg = 'An error occurred and the bookmarklet could not be loaded.'
+    return window.alert(msg)
+  }, { once: true })
   src.src = path + id + '.js'
   d.head.appendChild(src)
 }(document, 'script', '__ID__', '__PATH__'))
