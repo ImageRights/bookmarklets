f1fffda51bc566712c880b093095379d8369960e
Will Harney <will.harney@imagerights.com>
2018-10-15 09:44:19 -0400

    Update loader to catch more errors.

--- loader/loader.js
+++ loader/loader.js
@@ -1,9 +1,13 @@
 (function (d, s, id, path) {
-  var src = d.createElement(s)
-  src.addEventListener('error', function (evt) {
-    const msg = 'An error occurred and the bookmarklet could not be loaded.'
-    return window.alert(msg)
-  }, { once: true })
-  src.src = path + id + '.js'
-  d.head.appendChild(src)
+  function onError (e) {
+    window.alert('An error occurred and the bookmarklet could not be loaded.')
+  }
+  try {
+    var src = d.createElement(s)
+    src.addEventListener('error', onError, { once: true })
+    src.src = path + id + '.js'
+    d.head.appendChild(src)
+  } catch (e) {
+    onError(e)
+  }
 }(document, 'script', '__ID__', '__PATH__'))
