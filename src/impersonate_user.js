ec94bb688608461400f9658041b93dd22407b5b3
Will Harney <will.harney@imagerights.com>
2019-03-18 15:12:40 -0400

    Add text search to user impersonation loader.

--- src/impersonate_user.js
+++ src/impersonate_user.js
@@ -1,12 +1,31 @@
-;(function (id) {
+;(async function () {
   'use strict'
-  id = window.getSelection().toString().trim()
-  if (id) {
-    if (/\D/.test(id)) {
-      return alert('Selected text is not a number.')
-    }
-  } else {
-    id = window.prompt('User #').trim()
+  const nonDigit = n => /\D/.test(n)
+  let id = getSelection().toString().trim()
+  if (id && nonDigit(id)) {
+    return alert('Selected text is not a number.')
   }
-  location.href = 'https://www.imagerights.com/admin_impersonation/' + id
-}())
+  const IR = 'https://www.imagerights.com'
+  const isIR = location.origin === IR
+  id = prompt(`Enter a user ID${isIR ? ' or search query' : ''}:`)
+  id = id && id.trim()
+  if (!id) {
+    return
+  }
+  if (nonDigit(id)) {
+    if (!isIR) {
+      return alert('Entered text is not a number.')
+    }
+    const query = encodeURIComponent(id)
+    const url = `${IR}/api/v1/admin/users?filter=${query}&per_page=2`
+    const users = await fetch(url).then(res => res.json())
+    const active = users.filter(u => u.active)
+    if (active.length === 1) {
+      id = active[0].id
+    } else {
+      location.href = `admin#/users?q=${query}`
+      return
+    }
+  } // else ID is number; use as is
+  location.href = `${IR}/admin_impersonation/${id}`
+})()
